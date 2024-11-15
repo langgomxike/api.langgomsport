2873e92fce1c56a106c6540c997213e338d95b72
khanhcan2k4 <levietkhanh2k4@gmail.com>
2024-11-15 22:19:19 +0700

    change cors url, add before pull dev

--- src/main/java/com/langgomsport/langgomsport/controller/SizesController.java
+++ src/main/java/com/langgomsport/langgomsport/controller/SizesController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sizes")
-@CrossOrigin
+@CrossOrigin (origins = "http://127.0.0.1:3000")
 public class SizesController {
     @Autowired
     private SizeService sizeService;
