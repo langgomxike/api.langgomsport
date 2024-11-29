f31bfccc6afaef6cfeaba01426db7afad0ad4586
trkhang09 <dtrikhang0910@gmail.com>
2024-11-29 21:50:21 +0700

    change response Data in collections to CollectionDTO

--- src/main/java/com/langgomsport/langgomsport/controller/CollectionsController.java
+++ src/main/java/com/langgomsport/langgomsport/controller/CollectionsController.java
@@ -1,7 +1,12 @@
 package com.langgomsport.langgomsport.controller;
 
+import com.langgomsport.langgomsport.dtos.ResponseDTO.CollectionDTO;
+import com.langgomsport.langgomsport.dtos.ResponseDTO.ProductDTO;
 import com.langgomsport.langgomsport.models.Collection;
+import com.langgomsport.langgomsport.models.Product;
+import com.langgomsport.langgomsport.models.VariantImage;
 import com.langgomsport.langgomsport.service.CollectionService;
+import com.langgomsport.langgomsport.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,9 +24,23 @@ public class CollectionsController {
     @Autowired
     private CollectionService collectionService;
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping
-    public ResponseEntity<List<Collection>> getAllCollections(){
+    public ResponseEntity<List<CollectionDTO>> getAllCollections(){
+        List<CollectionDTO> collectionsResponse = new ArrayList<>();
         List<Collection> collections = collectionService.getAllCollections();
-        return ResponseEntity.ok(collections);
+        for(Collection collection : collections){
+            List<ProductDTO> productDTOs = new ArrayList<>();
+            for(Product product : collection.getProducts()){
+                List<VariantImage> images = productService.getImagesProducts(product);
+                ProductDTO productDTO = new ProductDTO(product, images);
+                productDTOs.add(productDTO);
+            }
+            CollectionDTO collectionDTO = new CollectionDTO(collection.getId(), collection.getName(), collection.getEnName(), collection.getImage(), collection.getLimitProduct(), productDTOs);
+            collectionsResponse.add(collectionDTO);
+        }
+        return ResponseEntity.ok(collectionsResponse);
     }
 }
